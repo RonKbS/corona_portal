ceb137f8de3da6882807462e2a3781e0223d2756
lynnnakiganda <lynnnakigandasekajugo@gmail.com>
2020-04-15 18:54:53 +0300

    filters map edits

--- js/africa_map.js
+++ js/africa_map.js
@@ -1,6 +1,6 @@
 var map = L.map('map', {
-  minZoom: 4
-}).setView([1.8, 22.24], 2);
+  minZoom: 3.4
+}).setView([1.8, -10.24], 2);
 
 
 function setParent(el, newParent) {
@@ -13,16 +13,16 @@ function setParent(el, newParent) {
 // }).addTo(map);
 
 var CartoDB_PositronNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	subdomains: 'abcd',
-	maxZoom: 19
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+  subdomains: 'abcd',
+  maxZoom: 19
 }).addTo(map);
 
 var africa_data = new L.GeoJSON(africa_data, {
   style: {
     weight: 2,
     opacity: 2,
-    color: '#4a4949',
+    color: '#e15b26',
     fillOpacity: 2.5,
     fillColor: '#ffffff00'
   },
@@ -40,7 +40,7 @@ var africa_data = new L.GeoJSON(africa_data, {
       this.setStyle({
         weight: 2,
         opacity: 2,
-        color: '#4a4949',
+        color: '#e15b26',
         fillOpacity: 2.5,
         fillColor: '#ffffff00'
       });
@@ -56,3 +56,27 @@ africa_data.eachLayer(function(layer) {
     this.closePopup();
   });
 });
+
+var info = L.control();
+
+info.onAdd = function(map) {
+  this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+  this.update();
+  return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function(props) {
+  this._div.innerHTML = '<h4>US Population Density</h4>' + (props ?
+    '<b>' + props.feature.properties.COUNTRY + '</b><br />' + props.feature.properties.POP + ' people / mi<sup>2</sup>' :
+    'Hover over a state');
+};
+
+info.addTo(map);
+function highlightFeature(e) {
+    info.update(layer.feature.properties);
+}
+
+function resetHighlight(e) {
+    info.update();
+}
