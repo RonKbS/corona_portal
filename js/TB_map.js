b35fa404878760b4dbfad881f43f264338135605
RonKbS <stephen.bamwite@gmail.com>
2020-04-17 18:46:19 +0300

    get TB numbers on first map

--- js/TB_map.js
+++ js/TB_map.js
@@ -1,16 +1,3 @@
-let long_id = "1tRF8gjyRd0oA2sSpTKmZqambggZzUM0YiED6KqF8H8M"
-let gid = "1502462034"
-let url = `https://docs.google.com/spreadsheets/d/${long_id}/export?format=csv&id=${long_id}&gid=${gid}`
-
-let map = L.map('map', {
-  minZoom: 3.4
-}).setView([1.8, -10.24], 2);
-
-L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-  subdomains: 'abcd',
-  maxZoom: 19
-}).addTo(map);
 
 function getColorTB(d) {
   return d > 86 ? '#404040' :
@@ -22,19 +9,20 @@ function getColorTB(d) {
     '#ffffff00';
 }
 
-axios.get(url)
-  .then(responseArrs => {
-    icus_data = $.csv.toObjects(responseArrs.data);
-    let icus_obj = {}
-    icus_data.forEach(object_ => {
-      icus_obj[object_["COUNTRY"]] = [
+let tb_stats_layer = () => {
+  if (african_data._map) {
+    map.removeLayer(african_data)
+  }
+  let tb_stats_obj = {}
+  google_sheet_data.forEach(object_ => {
+      tb_stats_obj[object_["COUNTRY"]] = [
         object_["POP"],
         object_["TB_rates"],
         object_["TB_percentage"]
       ]
     })
 
-    let african_data = L.geoJson(africa_data, {
+    african_data = L.geoJson(africa_data, {
       style: styleTB
     }).addTo(map);
 
@@ -42,9 +30,9 @@ axios.get(url)
       let country_ = layer.feature.properties.COUNTRY;
       layer.bindPopup(
         '<strong>Country:</strong> ' + country_
-        + '<br>' + '<strong>Population:</strong> ' + icus_obj[country_][0]
-        + '<br>' + '<strong>TB rates:</strong> ' + icus_obj[country_][1]
-        + '<br>' + '<strong>TB percentage:</strong> ' + icus_obj[country_][2]
+        + '<br>' + '<strong>Population:</strong> ' + tb_stats_obj[country_][0]
+        + '<br>' + '<strong>TB rates:</strong> ' + tb_stats_obj[country_][1]
+        + '<br>' + '<strong>TB percentage:</strong> ' + tb_stats_obj[country_][2]
       );
       layer.on('mouseover', function (e) {
         this.openPopup();
@@ -56,7 +44,7 @@ axios.get(url)
 
     function styleTB(feature) {
       return {
-        fillColor: getColorTB(parseFloat(icus_obj[feature.properties.COUNTRY][2].split(",").join(""))),
+        fillColor: getColorTB(parseFloat(tb_stats_obj[feature.properties.COUNTRY][2].split(",").join(""))),
         weight: 1,
         opacity: 1,
         color: 'black',
@@ -64,18 +52,13 @@ axios.get(url)
         fillOpacity: 1
       };
     }
-  })
 
-  var info = L.control();
-  info.onAdd = function(map) {
-    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
-    this.update();
-    return this._div;
-  };
-
-  info.update = function(props) {
-    this._div.innerHTML = (props ?
-      '<b>' + '</b><br />' + ' ' :
-      'Hover over a country');
-  };
-  info.addTo(map);
+    let legend_parent = document.getElementsByClassName("legend")[0]
+    if (legend_parent.childNodes.length > 1) {
+      legend_parent.removeChild(legend_parent.childNodes[1])
+    }
+    let legend_child = document.createElement("IMG")
+    legend_child.setAttribute("src", "images/TB_legend.png");
+    legend_child.setAttribute("class", "TB")
+    legend_parent.appendChild(legend_child);
+  }
