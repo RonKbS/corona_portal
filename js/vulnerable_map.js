91857dd4bf32249e932f142247d90ad610f9bc44
RonKbS <stephen.bamwite@gmail.com>
2020-04-17 18:56:43 +0300

    get vulnerable numbers onto first map

--- js/vulnerable_map.js
+++ js/vulnerable_map.js
@@ -1,81 +1,65 @@
-let long_id = "1tRF8gjyRd0oA2sSpTKmZqambggZzUM0YiED6KqF8H8M"
-let gid = "1502462034"
-let url = `https://docs.google.com/spreadsheets/d/${long_id}/export?format=csv&id=${long_id}&gid=${gid}`
-
-let map = L.map('map', {
-  minZoom: 3.4
-}).setView([1.8, -10.24], 2);
-
-L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-  subdomains: 'abcd',
-  maxZoom: 19
-}).addTo(map);
 
 function getColorvulnerable(d) {
   return d > 12.94 ? '#2b83ba' :
     d > 0.27 ? '#abdda4' :
-    d > 0.19 ? '#ffffbf' :
-    d > 0.16 ? '#fdae61' :
-    d > 0.10 ? '#d7191c' :
-    d > 0 ? '#f1eef6' :
-    '#ffffff00';
+      d > 0.19 ? '#ffffbf' :
+        d > 0.16 ? '#fdae61' :
+          d > 0.10 ? '#d7191c' :
+            d > 0 ? '#f1eef6' :
+              '#ffffff00';
 }
 
-axios.get(url)
-  .then(responseArrs => {
-    icus_data = $.csv.toObjects(responseArrs.data);
-    let icus_obj = {}
-    icus_data.forEach(object_ => {
-      icus_obj[object_["COUNTRY"]] = [
-        object_["POP"],
-        object_["Vulnerable_rates"],
-        object_["Vulnerable_percentage"]
-      ]
-    })
-
-    let african_data = L.geoJson(africa_data, {
-      style: stylevulnerable
-    }).addTo(map);
-
-    african_data.eachLayer(function (layer) {
-      let country_ = layer.feature.properties.COUNTRY;
-      layer.bindPopup(
-        '<strong>Country:</strong> ' + country_
-        + '<br>' + '<strong>Population:</strong> ' + icus_obj[country_][0]
-        + '<br>' + '<strong>Vulnerables with known HIV/AIDS $ TB cases rates:</strong> ' + icus_obj[country_][1]
-        + '<br>' + '<strong>Vulnerables with known HIV/AIDS $ TB cases percentage:</strong> ' + icus_obj[country_][2]
-      );
-      layer.on('mouseover', function (e) {
-        this.openPopup();
-      });
-      layer.on('mouseout', function (e) {
-        this.closePopup();
-      });
-    });
-
-    function stylevulnerable(feature) {
-      return {
-        fillColor: getColorvulnerable(parseFloat(icus_obj[feature.properties.COUNTRY][2].split(",").join(""))),
-        weight: 1,
-        opacity: 1,
-        color: 'black',
-        dashArray: '0',
-        fillOpacity: 1
-      };
-    }
+let vulnerable_stats_layer = () => {
+  if (african_data._map) {
+    map.removeLayer(african_data)
+  }
+  let vulnerable_stats_obj = {}
+  google_sheet_data.forEach(object_ => {
+    vulnerable_stats_obj[object_["COUNTRY"]] = [
+      object_["POP"],
+      object_["Vulnerable_rates"],
+      object_["Vulnerable_percentage"]
+    ]
   })
 
-  var info = L.control();
-  info.onAdd = function(map) {
-    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
-    this.update();
-    return this._div;
-  };
+  african_data = L.geoJson(africa_data, {
+    style: stylevulnerable
+  }).addTo(map);
 
-  info.update = function(props) {
-    this._div.innerHTML = (props ?
-      '<b>' + '</b><br />' + ' ' :
-      'Hover over a country');
-  };
-  info.addTo(map);
+  african_data.eachLayer(function (layer) {
+    let country_ = layer.feature.properties.COUNTRY;
+    layer.bindPopup(
+      '<strong>Country:</strong> ' + country_
+      + '<br>' + '<strong>Population:</strong> ' + vulnerable_stats_obj[country_][0]
+      + '<br>' + '<strong>Vulnerables with known HIV/AIDS $ TB cases rates:</strong> ' + vulnerable_stats_obj[country_][1]
+      + '<br>' + '<strong>Vulnerables with known HIV/AIDS $ TB cases percentage:</strong> ' + vulnerable_stats_obj[country_][2]
+    );
+    layer.on('mouseover', function (e) {
+      this.openPopup();
+    });
+    layer.on('mouseout', function (e) {
+      this.closePopup();
+    });
+  });
+
+  function stylevulnerable(feature) {
+    return {
+      fillColor: getColorvulnerable(parseFloat(vulnerable_stats_obj[feature.properties.COUNTRY][2].split(",").join(""))),
+      weight: 1,
+      opacity: 1,
+      color: 'black',
+      dashArray: '0',
+      fillOpacity: 1
+    };
+  }
+
+  let legend_parent = document.getElementsByClassName("legend")[0]
+  if (legend_parent.childNodes.length > 1) {
+    legend_parent.removeChild(legend_parent.childNodes[1])
+  }
+  let legend_child = document.createElement("IMG")
+  legend_child.setAttribute("src", "images/vulnerable_legend.png");
+  legend_child.setAttribute("class", "vulnerable")
+  legend_parent.appendChild(legend_child);
+}
+
