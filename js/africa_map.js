bf9c26249f2d354eb894f744e6a8e7b0b4973588
lynnnakiganda <lynnnakigandasekajugo@gmail.com>
2020-04-22 11:34:15 +0300

    popup for govt worked on, first map switched to cases map

--- js/africa_map.js
+++ js/africa_map.js
@@ -19,35 +19,16 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png
   maxZoom: 19
 }).addTo(map);
 
-let african_data = L.geoJson(africa_data, {
-  style: {
-    weight: 2,
-    opacity: 2,
-    color: '#e15b26',
-    fillOpacity: 2.5,
-    fillColor: '#ffffff00'
-  },
-  onEachFeature: function(feature, layer) {
-    layer.on('mouseover', function() {
-      this.setStyle({
-        weight: 2,
-        opacity: 2,
-        color: '#989898',
-        fillOpacity: 10,
-        fillColor: '#989898'
-      });
-    });
-    layer.on('mouseout', function() {
-      this.setStyle({
-        weight: 2,
-        opacity: 2,
-        color: '#e15b26',
-        fillOpacity: 2.5,
-        fillColor: '#ffffff00'
-      });
-    });
-  }
-}).addTo(map);
+let african_data;
+function getColorcases(d) {
+  return d > 2000 ? '#016c59' :
+  d > 250 ? '#1c9099' :
+    d > 50 ? '#67a9cf' :
+      d > 20 ? '#bdc9e1' :
+        d > 10 ? '#f6eff7' :
+        d > 0 ? '#f0f5f7' :
+            '#ffffff00';
+}
 
 
 axios.get(url)
@@ -57,16 +38,20 @@ axios.get(url)
     google_sheet_data.forEach(object_ => {
       initial_data_obj[object_["COUNTRY"]] = [
         object_["POP"],
-        object_["DENSITY"]
+        object_["CASES"]
       ]
     })
+
+    african_data = L.geoJson(africa_data, {
+      style: stylecases
+    }).addTo(map);
 
     african_data.eachLayer(function(layer) {
       let country_ = layer.feature.properties.COUNTRY;
       layer.bindPopup(
         '<strong>Country:</strong> ' + country_ +
         '<br>' + '<strong>Population:</strong> ' + initial_data_obj[country_][0] +
-        '<br>' + '<strong>Density:</strong> ' + initial_data_obj[country_][1]
+        '<br>' + '<strong>Cases:</strong> ' + initial_data_obj[country_][1]
       );
       layer.on('mouseover', function(e) {
         this.openPopup();
@@ -75,4 +60,20 @@ axios.get(url)
         this.closePopup();
       });
     });
+
+    function stylecases(feature) {
+      return {
+        fillColor: getColorcases(parseFloat(initial_data_obj[feature.properties.COUNTRY][1].split(",").join(""))),
+        weight: 1,
+        opacity: 1,
+        color: 'black',
+        dashArray: '0',
+        fillOpacity: 1
+      };
+    }
+    let legend_parent = document.getElementsByClassName("legend")[0]
+    let legend_child = document.createElement("IMG")
+    legend_child.setAttribute("src", "images/cases_legend.png");
+    legend_child.setAttribute("class", "cases")
+    legend_parent.appendChild(legend_child);
   })
