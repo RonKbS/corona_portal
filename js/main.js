8c90d6913789e0379108fd523817330da0cb3d61
RonKbS <stephen.bamwite@gmail.com>
2020-04-29 11:54:36 +0300

    use responsive navbar

--- js/main.js
+++ js/main.js
@@ -17,15 +17,3 @@
   });
 
 })(jQuery);
-
-var body = document.getElementsByTagName('body')[0],
-    sidebar = document.getElementById('sidebar');
-
-// sidebar overflow hide
-sidebar.onmouseover = function() { 
-    body.style.overflow = 'hidden';
-}
-
-sidebar.onmouseout = function() { 
-    body.style.overflow = 'auto';
-}
