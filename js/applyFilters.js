63a745b2a4a8976ece50aca2192895cddaa1a9ff
desire-geogecko <desire@geogecko.com>
2020-04-30 17:43:03 +0300

    cumulative_filters_v1

--- js/applyFilters.js
+++ js/applyFilters.js
@@ -1,3 +1,35 @@
 function filterMap() {
-    console.log(dataToFilter);
+    var arr = [
+        "False"
+    ];
+
+    for (key in map["_layers"]) {
+        if (
+            typeof map["_layers"][key]["feature"] !== "undefined" &&
+            map["_layers"][key]["feature"]["geometry"]["type"] === "MultiPolygon"
+        ) {
+            let filter = [];
+            var l = map["_layers"][key];
+            Object.keys(filterData[0]).slice(1).forEach((element, index) => {
+                let value = filter_data_obj[l.feature.properties.COUNTRY][index].split(",").join("");
+                if (parseFloat(value) > dataToFilter[element][0] && parseFloat(value) < dataToFilter[element][1]) {
+                    filter.push("True");
+                } else filter.push("False")
+            });
+            const found = filter
+                .some(r => arr.includes(r));
+              if (found) {
+                l.setStyle({
+                  opacity: 0,
+                  fillOpacity: 0
+                });
+              } else {
+                l.setStyle({
+                  opacity: 1,
+                  fillOpacity: 1
+                });
+              }
+        }
+    }
+
 }
--- js/createFilters.js
+++ js/createFilters.js
@@ -1,7 +1,69 @@
 let slider = document.getElementById('sliders');
 let filterData;
 
+let filter_data_obj ={};
+
 var dataToFilter = {}
+
+sidebar.on("content", (e) => {
+    if(e.id === 'profile' ) {
+        if (african_data._map) {
+            map.removeLayer(african_data)
+          }
+          
+          function getColorFilters() {
+              return {
+                weight: 1,
+                fillOpacity: 1,
+                fillColor: "#e15b26",
+            }
+          }
+
+          african_data = L.geoJson(africa_data, {
+            style: getColorFilters,
+          }).addTo(map);
+
+
+          addLegend([1], getColorFilters, "Filtered");
+
+    } else if (e.id === 'layers' ) {
+        if (african_data._map) {
+            map.removeLayer(african_data)
+          }
+  
+          african_data = L.geoJson(africa_data, {
+            style: stylecases
+          }).addTo(map);
+      
+          african_data.eachLayer(function (layer) {
+            let country_ = layer.feature.properties.COUNTRY;
+            
+            layer.bindPopup(
+              '<strong>Country:</strong> ' + country_ +
+              '<br>' + '<strong>Population:</strong> ' + initial_data_obj[country_][0] +
+              '<br>' + '<strong>Cases:</strong> ' + initial_data_obj[country_][1]
+            );
+            layer.on('mouseover', function (e) {
+              this.openPopup();
+            });
+            layer.on('mouseout', function (e) {
+              this.closePopup();
+            });
+          });
+      
+          function stylecases(feature) {
+            return {
+              fillColor: getColorcases(parseFloat(initial_data_obj[feature.properties.COUNTRY][1].split(",").join(""))),
+              weight: 1,
+              opacity: 1,
+              color: 'black',
+              dashArray: '0',
+              fillOpacity: 1
+            };
+          }
+          addLegend([0, 10, 20, 50, 250, 2000], getColorcases, "Cases");
+    }
+})
 
 
 function createSlider(filterName, min, max) {
@@ -50,6 +112,25 @@ axios.get(url1)
             let min = Infinity, max = -Infinity;
             if (element != "COUNTRY") {
                 filterData.forEach(object_ => {
+                    filter_data_obj[object_["COUNTRY"]] = [
+                        object_["DENSITY"],
+                        object_["CASES"], 
+                        object_["POP"], 
+                        object_["CASES_PER_100,000"], 
+                        object_["DEATHS"], 
+                        object_["DEATHS_PER_100,000"], 
+                        object_["ICU"], 
+                        object_["PEOPLE_PER_ICU"], 
+                        object_["VENTILATORS"], 
+                        object_["PEOPLE_PER_VENT"], 
+                        object_["HIV_rates"], 
+                        object_["HIV_percentage"], 
+                        object_["TB_rates"], 
+                        object_["TB_percentage"], 
+                        object_["Elderly_rates"], 
+                        object_["Elderly_percentage"], 
+                        object_["GDP_percentage"],
+                    ]
                     let value = object_[element].split(",").join("");
                         if (parseFloat(value) < min) min = parseFloat(value);
                         if (parseFloat(value) > max) max = parseFloat(value);
